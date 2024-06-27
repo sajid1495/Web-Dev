5484247b0669b59a19f524833b3c62ffaf24456d
sajid1495 <m.sajid1495@gmail.com>
2024-06-27 11:53:34 +0600

    Uploading Score Counter App

---
+++ Score Counter/index.js
@@ -0,0 +1,31 @@
+let hmCt = 0, gstCt = 0
+
+function hmBtn1(){
+    hmCt += 1
+    document.getElementById("hm-scr").textContent = hmCt
+}
+
+function hmBtn2(){
+    hmCt += 2
+    document.getElementById("hm-scr").textContent = hmCt
+}
+
+function hmBtn3(){
+    hmCt += 3
+    document.getElementById("hm-scr").textContent = hmCt
+}
+
+function gstBtn1(){
+    gstCt += 1
+    document.getElementById("gst-scr").textContent = gstCt
+}
+
+function gstBtn2(){
+    gstCt += 2
+    document.getElementById("gst-scr").textContent = gstCt
+}
+
+function gstBtn3(){
+    gstCt += 3
+    document.getElementById("gst-scr").textContent = gstCt
+}
