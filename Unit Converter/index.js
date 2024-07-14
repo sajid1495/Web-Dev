fc2ffe2c434442bba68004581b60ecef46f949e9
MD.SAJEDUL ISLAM <m.sajid1495@gmail.com>
2024-07-14 12:43:37 +0600

    Adding Unit Converter

---
+++ Unit Converter/index.js
@@ -0,0 +1,22 @@
+let inputEl = document.getElementById("input-el")
+const convertBtn = document.getElementById("convert-btn")
+
+convertBtn.addEventListener("click", function() {
+    let inputVal = Number(inputEl.value) 
+
+    let mTf = (inputVal * 3.28084).toFixed(3) 
+    let fTm = (inputVal * 0.3048).toFixed(3)
+    let outputEl = document.getElementById("output-length")
+    outputEl.innerText = `${inputVal} meters = ${mTf} feets | ${inputVal} feets = ${fTm} meters`
+
+    let lTg = (inputVal * 0.264172).toFixed(3)
+    let gTl = (inputVal * 3.78541).toFixed(3)
+    let outputEl2 = document.getElementById("output-volume")
+    outputEl2.innerText = `${inputVal} liters = ${lTg} gallons | ${inputVal} gallons = ${gTl} liters`
+
+    let kTp = (inputVal * 2.20462).toFixed(3)
+    let pTk = (inputVal * 0.453592).toFixed(3)
+    let outputEl3 = document.getElementById("output-mass")
+    outputEl3.innerText = `${inputVal} kilograms = ${kTp} pounds | ${inputVal} pounds = ${pTk} kilograms`
+
+})
